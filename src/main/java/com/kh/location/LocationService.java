97bb8de7c14fa208dcfd937458465738963ca9be
bomblike611 <rhtn611@naver.com>
2018-02-08 17:31:42 +0900

    s
    s

--- src/main/java/com/kh/location/LocationService.java
+++ src/main/java/com/kh/location/LocationService.java
@@ -8,13 +8,16 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class LocationService {
 
-	public int locationInsert(LocationDTO locationDTO,HttpSession session,MultipartFile [] file){
+	public int locationInsert(LocationDTO locationDTO,HttpSession session,MultipartFile [] file) throws Exception{
 		return 0;
 	}
 	
-	public int locationUpdate(LocationDTO locationDTO,HttpSession session,MultipartFile [] file){
+	public int locationUpdate(LocationDTO locationDTO,HttpSession session,MultipartFile [] file) throws Exception{
 		return 0;
 	}
 	
+	public int locationDelete(LocationDTO locationDTO) throws Exception{
+		return 0;
+	}
 	
 }
