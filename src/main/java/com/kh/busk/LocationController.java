d4f67ea2eed2eb606edda9b4e786b05c3cd96577
bomblike611 <rhtn611@naver.com>
2018-02-08 18:07:03 +0900

    w
    e

--- src/main/java/com/kh/busk/LocationController.java
+++ src/main/java/com/kh/busk/LocationController.java
@@ -1,15 +1,20 @@
 package com.kh.busk;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.location.LocationDTO;
 
 @Controller
 @RequestMapping(value="/location/**")
 public class LocationController {
 	
 	@RequestMapping(value="locationWrite",method=RequestMethod.GET)
-	public void locationWrite() throws Exception{	
+	public void locationWrite(LocationDTO locationDTO,MultipartFile [] files,HttpSession session) throws Exception{	
 	}
 	
 	@RequestMapping(value="locationList")
--- src/main/java/com/kh/location/LocationService.java
+++ src/main/java/com/kh/location/LocationService.java
@@ -1,5 +1,8 @@
 package com.kh.location;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class LocationService {
+	
+	@Inject
+	private LocationDAO locationDAO;
 
 	public int locationInsert(LocationDTO locationDTO,HttpSession session,MultipartFile [] file) throws Exception{
 		return 0;
@@ -18,6 +24,10 @@ public class LocationService {
 	
 	public int locationDelete(LocationDTO locationDTO) throws Exception{
 		return 0;
+	}
+	
+	public List<LocationDTO> locationList() throws Exception{
+		return null;
 	}
 	
 }
