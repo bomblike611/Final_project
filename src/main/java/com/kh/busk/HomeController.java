154a84f1f8c3a5f77e2a8d1063a036828fcda260
bomblike611 <rhtn611@naver.com>
2018-02-09 19:01:48 +0900

    ㄴ
    ㄴ

--- src/main/java/com/kh/busk/HomeController.java
+++ src/main/java/com/kh/busk/HomeController.java
@@ -15,6 +15,8 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public void home() {}
+	public String home() {
+		return "home";
+	}
 	
 }
